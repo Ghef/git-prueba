eb9ad16b6167a1fdb066ab6e3b1d10b611d70a0f
Ghef <ghef26@gmail.com>
2022-06-04 23:22:46 -0500

    boogie y lola duermen

---
+++ pollo.py
@@ -0,0 +1,11 @@
+pollo = input("como esta el pollo : ")
+
+if (pollo == "vivo"):
+    print("el pollo esta muerto")
+
+elif (pollo == "muerto"):
+    print("el pollo esta vivo")
+
+else:
+    print("el pollo esta caro")
+    
